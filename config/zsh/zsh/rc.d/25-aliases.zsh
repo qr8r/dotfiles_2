cd3575fd01f6cce0456cfc7da84bd3306b879c02
Brendan Deere <brendan.deere@gmail.com>
2026-02-22 16:29:15 -0500

    Add an alias to force rebuild the antigen bundle after making changes

--- config/zsh/zsh/rc.d/25-aliases.zsh
+++ config/zsh/zsh/rc.d/25-aliases.zsh
@@ -3,3 +3,7 @@
 if (( $+commands[yq] )); then
   alias jq='yq'
 fi
+
+# Rebuild antidote's static plugin bundle after plugin list changes.
+# This bypasses stale mtime checks and restarts a clean login shell.
+alias zrebundle='source ~/.local/share/antidote/antidote.zsh && antidote bundle < ~/.config/zsh/antidote.plugins >| ~/.local/share/antidote/antidote-bundle.zsh && exec zsh -l'
